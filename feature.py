5766feb1620173ed207fa01ba83b4b0ee604010a
Sankalp Sangle <sanku.sangle@gmail.com>
2022-04-12 01:01:13 -0400

    Q4 Programming part done. One-liner LOL.

--- feature.py
+++ feature.py
@@ -12,6 +12,17 @@ def create_nl_feature(X):
     '''
     
     #TODO: implement this
+    N = X.shape[0]
+    d = X.shape[1]
+    X_new = np.zeros((N, d + 1))
+    X_new[:, :-1] = X
+
+    for i in range(N):
+        if (X_new[i][0] <= 1 and X_new[i][1] <= 1) or (X_new[i][0] > 1 and X_new[i][1] > 1):
+            X_new[i][2] = -1
+        else:
+            X_new[i][2] = 1
+    return X_new
 
 
     
